48de9608448b9374a2ba8dcc6480ba9d03dd4347
David Goldstein <goldstein.g.david@gmail.com>
2023-06-28 21:28:29 -0400

    Add a title panel to the plugin panel

--- src/main/java/com/onscreennotes/ui/OnScreenNotesPanel.java
+++ src/main/java/com/onscreennotes/ui/OnScreenNotesPanel.java
@@ -2,20 +2,37 @@ package com.onscreennotes.ui;
 
 import com.onscreennotes.OnScreenNotesConfig;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.plugins.screenmarkers.ScreenMarkerPlugin;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+import net.runelite.client.util.ImageUtil;
 
 @Slf4j
 public class OnScreenNotesPanel extends PluginPanel
 {
+	private static final ImageIcon ADD_ICON;
+
+	static
+	{
+		final BufferedImage addIcon = ImageUtil.loadImageResource(ScreenMarkerPlugin.class, "add_icon.png");
+		ADD_ICON = new ImageIcon(addIcon);
+	}
 	public void init(OnScreenNotesConfig config)
 	{
 		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+		JPanel northPanel = buildNorthPanel();
+		add(northPanel, BorderLayout.NORTH);
+
 		setBackground(ColorScheme.BRAND_ORANGE);
 
 		JPanel notesContainer = new JPanel();
@@ -25,5 +42,21 @@ public class OnScreenNotesPanel extends PluginPanel
 		notesContainer.setBorder(new EmptyBorder(10, 10, 10, 10));
 
 		add(notesContainer, BorderLayout.CENTER);
+	}
+
+	private JPanel buildNorthPanel() {
+		JPanel northPanel = new JPanel(new BorderLayout());
+		northPanel.setBorder(new EmptyBorder(1, 0, 10, 0));
+
+		JLabel title = new JLabel();
+		title.setText("On Screen Notes");
+		title.setForeground(Color.WHITE);
+
+		JLabel addMarker = new JLabel(ADD_ICON);
+
+		northPanel.add(title, BorderLayout.WEST);
+		northPanel.add(addMarker, BorderLayout.EAST);
+
+		return northPanel;
 	}
 }
